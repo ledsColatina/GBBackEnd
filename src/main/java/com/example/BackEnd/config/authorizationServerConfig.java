4e4979a72fde45813c7a1e1d7d6b99edfa541630
Leds2 <jpedrocalixto@outlook.com>
2019-12-12 11:01:45 -0300

    Preparando para a poducao

--- src/main/java/com/example/BackEnd/config/authorizationServerConfig.java
+++ src/main/java/com/example/BackEnd/config/authorizationServerConfig.java
@@ -1,5 +1,10 @@
 package com.example.BackEnd.config;
 
+import org.springframework.context.annotation.Configuration;
+
+
+@Configuration
+//@EnableAuthorizationServer
 public class authorizationServerConfig {
 
 }
