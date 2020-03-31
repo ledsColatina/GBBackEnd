ed7188779ca86914a8dbef8e4b0a70d9c4d2496e
jpedroc <jpedrocalixto@outlook.com>
2020-03-31 11:10:42 -0300

    SPRINT06B- Adicionando put CapacidadeProducaoExtra

--- src/main/java/com/example/BackEnd/web/CapacidadeProducaoExtraResource.java
+++ src/main/java/com/example/BackEnd/web/CapacidadeProducaoExtraResource.java
@@ -2,12 +2,18 @@ package com.example.BackEnd.web;
 
 import java.util.List;
 
+import javax.servlet.http.HttpServletResponse;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.example.BackEnd.domain.CapacidadeProducao;
 import com.example.BackEnd.domain.CapacidadeProducaoExtra;
 
 
@@ -23,5 +29,18 @@ public class CapacidadeProducaoExtraResource {
 		List<CapacidadeProducaoExtra> capacidade = CapacidadeProducaoExtraRepository.findAll();	
 		return !capacidade.isEmpty() ? ResponseEntity.ok(capacidade) : ResponseEntity.noContent().build();	
 	}
+	
+	//-----------------------------------------------------------------------------------------------------------------------	
+	
+	@PutMapping("/{id}")
+	protected ResponseEntity<CapacidadeProducaoExtra> atualizaCapacidade(@PathVariable("id") Long id,  @RequestBody CapacidadeProducaoExtra capacidadeProducao,HttpServletResponse responseEntity) {
+		return CapacidadeProducaoExtraRepository.findById(id).map(record -> {
+			record.setCapacidadeHora(capacidadeProducao.getCapacidadeHora());
+			CapacidadeProducaoExtra updated = CapacidadeProducaoExtraRepository.save(record);
+			return ResponseEntity.ok().body(updated);
+
+		}).orElse(ResponseEntity.notFound().build());
+	}
+	
 	
 }
