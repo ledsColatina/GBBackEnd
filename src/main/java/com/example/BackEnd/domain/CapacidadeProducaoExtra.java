d7a3a5af2b8fb706dcba8f49464b9d22d5139251
Hercules <herculesfernandes13@gmail.com>
2020-06-29 10:18:38 -0300

    Colocando dependencia do javaJRE no backEnd

--- src/main/java/com/example/BackEnd/domain/CapacidadeProducaoExtra.java
+++ src/main/java/com/example/BackEnd/domain/CapacidadeProducaoExtra.java
@@ -8,14 +8,12 @@ import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
 
 import lombok.Data;
-import lombok.Getter;
-import lombok.Setter;
+
 
 
 @Entity
 //@DiscriminatorValue(value = "F")
-@Getter
-@Setter
+@Data
 public class CapacidadeProducaoExtra extends CapacidadeProducao {
 	
 
